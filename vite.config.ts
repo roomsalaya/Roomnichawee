d6e9181ae30628431c6c35c08952aad8b445fc87
Supunsapoon <supunsapoon@gmail.com>
2024-09-23 11:49:45 +0700

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Roomnichawee/',  // แก้ไขเป็นชื่อ repository ของคุณ
   plugins: [react()],
+  base: '/roomnichawee/',  // ชื่อ repository ของคุณ
 })
-
